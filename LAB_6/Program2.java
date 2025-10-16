ef13d7f51100b2e1bfdc2aae8d083c706c282a7b
Harsha Puranik <harshagopalkrishnapuranikbca24@rvu.edu.in>
2025-10-16 13:02:26 +0530

    Create Program2.java

---
+++ LAB_6/Program2.java
@@ -0,0 +1,11 @@
+public class WrapperExample {
+    public static void main(String[] args) {
+        int num = 10;                 
+        Integer obj = num;            
+        int value = obj;  
+      
+        System.out.println("Primitive value: " + num);
+        System.out.println("After Autoboxing (Object): " + obj);
+        System.out.println("After Auto-unboxing (Primitive again): " + value);
+    }
+}
